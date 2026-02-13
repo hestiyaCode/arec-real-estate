b542336ed4e08c63af303abe5b67fe9864455ac4
hestiyaCode <techsupport@gmail.com>
2026-02-13 17:12:05 +0530

    added web analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,8 +27,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <Analytics/>
+        
         {children}
+        <Analytics/>
       </body>
     </html>
   );
